a94b5fde0a708222403780b02af834a15d78bda2
pankajjangid05 <pankaj.jangid@aurigait.com>
2023-05-14 00:13:03 +0530

    init backend server

---
+++ src/main/java/com/gitpodbackend/GitpodBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gitpodbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitpodBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitpodBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gitpodbackend/GitpodBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gitpodbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitpodBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
